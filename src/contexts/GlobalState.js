af50cb69257283c5243c42faa6860e64eb87a954
Karen Gaspar <karenasgaspar@gmail.com>
2022-01-10 15:15:21 -0300

    Retirei estilos padrões com código em <style> no index.html da pasta public

--- src/contexts/GlobalState.js
+++ src/contexts/GlobalState.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { GlobalContext } from "../GlobalStateContext/GlobalStateContext";
 
 const GlobalState = (props) => {
